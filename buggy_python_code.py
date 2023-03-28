c8c93d0c3b49dcb249e96a2b915d7395c089222f
Simon Plhak <plhak.s@gmail.com>
2023-03-28 18:46:45 +0200

    fix: safer code

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,6 @@
-import sys 
-import os
-import yaml
 import flask
+import yaml
+from debugpy._vendored.pydevd.pydev_sitecustomize.sitecustomize import raw_input
 
 app = flask.Flask(__name__)
 
@@ -12,8 +11,10 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
+
+
 class Person(object):
     def __init__(self, name):
         self.name = name
@@ -27,41 +28,45 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
-    try: 
+
+    try:
         http = urllib.PoolManager()
-        r = http.request('GET', url)
+        r = http.request("GET", url)
     except:
-        print('Exception')
+        print("Exception")
 
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream)  # deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
-    assert password == "Iloveyou", "Invalid password!"
+    if password != "Iloveyou":
+        raise Exception('Invalid password')
     print("Successfully authenticated!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("Vulnerabilities:")
-    print("1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}")
+    print(
+        "1. Format string vulnerability: use string={person.__init__.__globals__[CONFIG][API_KEY]}"
+    )
     print("2. Code injection vulnerability: use string=;print('Own code executed') #")
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
-    choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
-        print_nametag(input("Please format your nametag: "), new_person)
+    choice = raw_input("Select vulnerability: ")
+    if choice == "1":
+        new_person = Person("Vickie")
+        print_nametag(raw_input("Please format your nametag: "), new_person)
     elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
+        urlib_version = raw_input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
-        load_yaml(input("File name: "))
+        load_yaml(raw_input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = input("Enter master password: ")
+        password = raw_input("Enter master password: ")
         authenticate(password)
-
